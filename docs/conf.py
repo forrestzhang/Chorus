a6bbaa2a59a72119409135df78955e8ac3e3b873
Tao Zhang <forrest_zhang@163.com>
2018-03-15 16:00:39 +0800

    update 2018

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Chorus'
-copyright = u'2016, Tao Zhang'
+copyright = u'2018, Tao Zhang'
 author = u'Tao Zhang'
 
 # The version info for the project you're documenting, acts as replacement for
